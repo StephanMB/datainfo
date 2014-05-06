7cc337246170568d693f9cd5553bff756bb527ed
StephanMB <StephanMB@users.noreply.github.com>
2014-05-06 17:01:57 +0200

    Shunting rule

--- session2/result3b.sql
+++ session2/result3b.sql
@@ -1,10 +1,8 @@
-SELECT p.pid FROM Person p 
-	WHERE EXISTS( 
-		SELECT * FROM Acts a 
-			WHERE a.pid = p.pid 
-			AND a.mid IN (
-						SELECT m.mid FROM Movie m 
-							WHERE m.name = 'Back to the Future'
-						)
+SELECT DISTINCT p.pid FROM Person p, Acts a,  
+	WHERE a.pid = p.pid 
+	AND a.mid IN (
+		SELECT m.mid FROM Movie m 
+			WHERE m.name = 'Back to the Future'
+	)
 );
 

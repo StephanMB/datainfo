dafdc51a19126d1ba97dc08e9548e1227fdea88c
StephanMB <stephan.braams@gmail.com>
2014-05-06 14:29:35 +0200

    Added src directory

---
+++ session0/src/main/java/nl/utwente/di/Quoter.java
@@ -0,0 +1,17 @@
+package nl.utwente.di;
+
+public class Quoter {
+
+	double getBookPrice(String s){
+		
+		switch (s) {
+		case "1": return 10.0;
+		case "2": return 45.0;
+		case "3": return 20.0;
+		case "4": return 35.0;
+		case "5": return 50.0;
+		default: return 0.0;
+		}
+
+	}
+}

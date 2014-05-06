9e3340c771bfc0ccb5e5bd2abb37f5c7e34347a5
StephanMB <stephan.braams@gmail.com>
2014-05-06 15:01:44 +0200

    Created ecercise1 & result1

---
+++ session2/result1.sql
@@ -0,0 +1,12 @@
+INSERT ALL
+	INTO Problem1(task, result, motivation) VALUES ('A', false,'Niet waar, verschillende voertuigen kunnen dezelfde capaciteit hebben')
+	INTO Problem1(task, result, motivation) VALUES ('B', true, 'Waar zie 3')
+	INTO Problem1(task, result, motivation) VALUES ('C', true,'Waar zie 7 en 8')
+	INTO Problem1(task, result, motivation) VALUES ('D', true,'Waar zie a) 7 c)')
+	INTO Problem1(task, result, motivation) VALUES ('E', true,'Waar zie a) en een pakje heeft één prijs')
+	INTO Problem1(task, result, motivation) VALUES ('F', false,'Niet waar, een bezorger kan voor verschillende voertuigen gemachtigt zijn')
+	INTO Problem1(task, result, motivation) VALUES ('G', false,'Niet waar meerdere bezorgers kunnen voor één voertuig gemachtigt zijn en de capaciteit van het voertuig heeft geen invloed')
+	INTO Problem1(task, result, motivation) VALUES ('H', true,'Waar zie G')
+	INTO Problem1(task, result, motivation) VALUES ('I', false,'Niet waar, een voertuig heeft maar één capaciteit')
+	INTO Problem1(task, result, motivation) VALUES ('J', false,' C is niet onafhankelijk van V')
+SELECT * FROM Problem1;
